2e1ebefe57f4a6000e315fbd97801ad34ba39c08
ekachner <ekibam@gmail.com>
2019-11-21 21:12:49 -0700

    using any asymmetrical container, I can figure out how much oil I need to make soap that will fill the mold exactly.

--- answer.js
+++ answer.js
@@ -4,13 +4,8 @@ const OUNCES_WATER_TO_CUBIC_INCHES = 9 / 5;
 const CUBIC_INCHES_TO_OUNCES_OIL = 0.39;
 const GRAMS_PER_OUNCE = 28.35;
 
-function waterToOil(ouncesOfWater){
-    return (ouncesOfWater * OUNCES_WATER_TO_CUBIC_INCHES * CUBIC_INCHES_TO_OUNCES_OIL).toFixed(2) + " oz. oil needed.";
+function waterToOil(gramsWater){
+    return (gramsWater / GRAMS_PER_OUNCE * OUNCES_WATER_TO_CUBIC_INCHES * CUBIC_INCHES_TO_OUNCES_OIL * GRAMS_PER_OUNCE).toFixed(1) + " grams oil needed.";
 }
 
-function ounceToGrams(ounces){
-    return "= " + ounces * GRAMS_PER_OUNCE + " grams.";
-}
-
-console.log(waterToOil(6.1));
-console.log(ounceToGrams(13));
+console.log(waterToOil(172.93));
