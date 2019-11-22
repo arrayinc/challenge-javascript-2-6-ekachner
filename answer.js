9a0d8f47cb7631a27ebce39fa54dc2676d257726
ekachner <ekibam@gmail.com>
2019-11-21 19:14:30 -0700

    challenge 6 complete

--- answer.js
+++ answer.js
@@ -1,1 +1,11 @@
 // *** YOUR ANSWER BELOW ***
+
+const OUNCES_WATER_TO_CUBIC_INCHES = 9 / 5;
+const CUBIC_INCHES_TO_OUNCES_OIL = 0.39;
+
+function convertWaterOuncesToOil(ouncesOfWater){
+    let cubicInches = ouncesOfWater * OUNCES_WATER_TO_CUBIC_INCHES;
+    let ouncesInOil = cubicInches * CUBIC_INCHES_TO_OUNCES_OIL;
+    return ouncesInOil;
+}
+console.log(convertWaterOuncesToOil(6.1));
